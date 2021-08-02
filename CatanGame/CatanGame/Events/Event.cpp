7b5bc4d0c467a5747f6d852ad2a7c14d15d68a56
eli <eli9850@gmail.com>
2021-08-02 10:36:09 +0300

    add events utils

---
+++ CatanGame/CatanGame/Events/Event.cpp
@@ -0,0 +1,25 @@
+#include "Event.h"
+#include "Exceptions/EventsExceptions.h"
+
+Event::Event() {
+	m_event = CreateEvent(nullptr, true, true, nullptr);
+	if (m_event == nullptr) {
+		throw CreateEventException("Failed to create event with error: " + std::to_string(GetLastError()));
+	}
+}
+
+void Event::set_event() {
+	if (!SetEvent(m_event)) {
+		throw SetEventException("Failed to set event with error: " + std::to_string(GetLastError()));
+	}
+}
+
+void Event::reset_event() {
+	if (!ResetEvent(m_event)) {
+		throw SetEventException("Failed to reset event with error: " + std::to_string(GetLastError()));
+	}
+}
+
+const HANDLE& Event::get_event() const{
+	return m_event;
+}
